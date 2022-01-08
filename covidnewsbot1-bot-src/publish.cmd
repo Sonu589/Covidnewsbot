23d8dc0667c154a77b1a9d29d90d1028705e61f9
Sonu <78155991+Sonu589@users.noreply.github.com>
2022-01-08 23:09:15 +0530

    Add files via upload

---
+++ covidnewsbot1-bot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\covidnewsbot1-bot-9c71-Web-Deploy.pubxml -p:Password=hAftNrfqxmihsdcjfWG1yLLAkLcZG4tJ1jdkDF0nPijBQwX0vs0Wvrjj9jeF
+
